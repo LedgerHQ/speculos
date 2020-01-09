4c951399c3f9c744fe01519a3ad87905a9ea8990
greenknot <greenknot@users.noreply.github.com>
2020-01-09 10:08:08 +0100

    tools/debug.sh: always add build/src/launcher symbols

--- tools/debug.sh
+++ tools/debug.sh
@@ -2,23 +2,28 @@
 
 set -e
 
+function get_text_addr()
+{
+    local elf="$1"
+
+    arm-none-eabi-readelf -WS "$elf" \
+        | grep '\.text' \
+        | awk '{ print "0x"$5 }'
+}
+
 function main()
 {
     local app="$1"
-
-    if [[ "${app}" == *"/launcher" ]]; then
-        # special case for launcher binary
-        local add_symbol="add-symbol-file \"${app}\" 0x101c0"
-    else
-        local add_symbol="add-symbol-file \"${app}\" 0x40000000"$'\n'"b *0x40000000"
-    fi
+    local launcher_text_addr=$(get_text_addr "build/src/launcher")
 
     cat >/tmp/x.gdb<<EOF
 source ./tools/gdbinit
 set architecture arm
 target remote 127.0.0.1:1234
 handle SIGILL nostop pass noprint
-${add_symbol}
+add-symbol-file "build/src/launcher" ${launcher_text_addr}
+add-symbol-file "${app}" 0x40000000
+b *0x40000000
 c
 EOF
 
