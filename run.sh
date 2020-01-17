3044a582df521f646a6d60e266905503981e782d
Nabil Bourenane <bourenane.nabil@icloud.com>
2020-01-17 14:28:53 +0100

    enhanced parameters readability

--- run.sh
+++ run.sh
@@ -4,4 +4,13 @@ set -euxo pipefail
 source $(pipenv --venv)/bin/activate
 websockify 50000 localhost:40000 --cert ./certificate/ws.pem &  # for apdu
 websockify 60000 localhost:41000 --cert ./certificate/ws.pem & # for vnc
-exec ./speculos.py ${DEBUG_MODE:-} -m ${DEVICE_MODEL} ./apps/${APP_FILE} --sdk ${SDK_VERSION} --seed "${DEVICE_SEED}" ${EXTRA_OPTIONS:-} --headless --vnc-port 41000 --apdu-port 40000
+
+exec ./speculos.py ${DEBUG_MODE:-} \
+    -m ${DEVICE_MODEL} \
+    ./apps/${APP_FILE} \
+    --sdk ${SDK_VERSION} \
+    --seed "${DEVICE_SEED}" \
+    ${EXTRA_OPTIONS:-} \
+    --headless \
+    --vnc-port 41000 \
+    --apdu-port 40000
