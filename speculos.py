LedgerHQ/speculos

commit a6f7b65f32c0cd7a6fbe6f48c82d279b5ce8b36d

merge ram page and ram size arguments into a single one
Instead of passing -r 0x1234000 -q 0x100, just pass -r 0x1234000:0x100.

speculos.py
@@ -10,6 +10,7 @@ import ctypes
 from elftools.elf.elffile import ELFFile
 from mnemonic import mnemonic
 import os
+import re
 import signal
 import socket
 import sys
@@ -54,39 +55,8 @@ def get_elf_infos(app_path):
     stack_size = estack - stack
     return sh_offset, sh_size, stack, stack_size, ram_addr, ram_size
 
-def sanitize_ram_args(model: str, rampage: str, pagesize: str, extra_ram: dict):
-    ''' Check RAM pages probed from main app & libs are identical and throw an error if it's
-    not the case. This is not generic but is sufficient as far as FW Apps team is concerned.
-    (Note: Generic code would merge overlapping pages and remove duplicate ones so that the 
-    list of pages passed to launcher contains unique items).  
-    Also discard extra RAM pages probed from main app or libraries when not running on Blue
-    '''
-    args = []
-    origin = ['*', 'probed']
-    if rampage is not None and pagesize is not None:    # then user-provided values supercede probed values
-        if extra_ram is None:
-            extra_ram = [{}]
-        # First extra_ram list entry is main app extra RAM params, replace it by user-provided {rampage, pagesize}
-        _ = extra_ram.pop(0)
-        extra_ram.insert(0, {'addr':rampage, 'size':pagesize})
-        origin=['+', 'user-provided']
-
-    if extra_ram:
-        if  len(extra_ram) > 1:     # then all pages shall be identical
-            # Remove identical extra ram params,the python way. Throw an error if resulting list length is not 1
-            extra_ram = [dict(s) for s in set(tuple(d.items()) for d in extra_ram)]
-            if len(extra_ram) > 1:
-                print("[-] Error: different extra RAM pages for main app and/or libraries!")
-                sys.exit(1)
-        page = extra_ram[0]
-        print(f'[{origin[0]}] using {origin[1]} {page["size"]}-byte additional RAM page @0x{hex(page["addr"])}')
-        args.extend(['-r', hex(page["addr"])] + ['-s', hex(page["size"])])
-    return args     # either [] or ['-r', '<adress>', '-s', '<size>']
-
-
-def run_qemu(s1, s2, app_path, libraries=[], seed=DEFAULT_SEED, debug=False, trace_syscalls=False, model=None, rampage=None, pagesize=None, sdk_version="1.5"):
+def run_qemu(s1, s2, app_path, libraries=[], seed=DEFAULT_SEED, debug=False, trace_syscalls=False, model=None, ram_arg=None, sdk_version="1.5"):
     args = [ 'qemu-arm-static' ]
-    extra_ram = []
 
     if debug:
         args += [ '-g', '1234', '-singlestep' ]
@@ -101,16 +71,23 @@ def run_qemu(s1, s2, app_path, libraries=[], seed=DEFAULT_SEED, debug=False, tra
 
     args += [ '-k', str(sdk_version) ]
 
+    extra_ram = ''
     for lib in [ f'main:{app_path}' ] + libraries:
         name, lib_path = lib.split(':')
         load_offset, load_size, stack, stack_size, ram_addr, ram_size = get_elf_infos(lib_path)
         # Since binaries loaded as libs could also declare extra RAM page(s), collect them all
         if (ram_addr, ram_size) != (0, 0):
-            extra_ram.append({'addr':ram_addr, 'size':ram_size})
+            arg = f'{ram_addr:#x}:{ram_size:#x}'
+            if extra_ram and arg != extra_ram:
+                print("[-] Error: different extra RAM pages for main app and/or libraries!")
+                sys.exit(1)
         args.append(f'{name}:{lib_path}:{hex(load_offset)}:{hex(load_size)}:{hex(stack)}:{hex(stack_size)}')
 
     if model == 'blue':
-        args.extend(sanitize_ram_args(model, rampage, pagesize, extra_ram))
+        if ram_arg:
+            extra_ram = ram_arg
+        if extra_ram:
+            args.extend([ '-r', extra_ram ])
 
     pid = os.fork()
     if pid != 0:
@@ -156,8 +133,7 @@ if __name__ == '__main__':
     group.add_argument('-o', '--ontop', action='store_true', help='The window stays on top of all other windows')
     parser.add_argument('-x', '--text', action='store_true', help="Text UI (implies --headless)")
     parser.add_argument('-y', '--keymap', action='store', help="Text UI keymap in the form of a string (e.g. 'was' => 'w' for left button, 'a' right, 's' both). Default: arrow keys")
-    parser.add_argument('-r', '--rampage', required='--pagesize' in sys.argv or '-q' in sys.argv, type=lambda x: int(x,16), action='store', help='Hex address (prefixed or not with \'0x\') of one additional RAM page available to the app. Requires -q. Supercedes the internal probing for such page.')
-    parser.add_argument('-q', '--pagesize', required='--rampage' in sys.argv or '-r' in sys.argv, type=lambda x: int(x,16), action='store', help='Byte size (in hexadecimal, prefixed or not with \'0x\') of the additional RAM page available to the app. Requires -r.  Supercedes the internal probing for such page.')
+    parser.add_argument('-r', '--rampage', help='Additional RAM page and size available to the app (eg. 0x123000:0x100). Supercedes the internal probing for such page.')
     group_progressive = parser.add_mutually_exclusive_group()
     group_progressive.add_argument('-p', '--progressive', action='store_true', help='Enable step-by-step rendering of graphical elements (default for Blue, can be disabled with -P)')
     group_progressive.add_argument('-P', '--flushed', action='store_true', help='Disable step-by-step rendering of graphical elements (default for Nano S)')
@@ -175,17 +151,20 @@ if __name__ == '__main__':
         else:
             args.flushed = True
 
-    if args.model != 'blue' and (args.rampage != None or args.pagesize != None):
-        # Extra RAM page only allowed on Blue device
-        print("Extra RAM page arguments -r (--rampage) & -q (--pagesize)) require '-m blue'")
-        sys.exit(1)
+    if args.rampage:
+        if args.model != 'blue':
+            print("[-] Extra RAM page arguments -r (--rampage) require '-m blue'", file=sys.stderr)
+            sys.exit(1)
+        if not re.match('(0x)?[0-9a-fA-F]+:(0x)?[0-9a-fA-F]+$', args.rampage):
+            print("[-] Invalid ram page argument", file=sys.stderr)
+            sys.exit(1)
 
     if args.text:
         if args.model != 'nanos':
             print(f"[-] Error: Unsupported model '{args.model}' with argument -x", file=sys.stderr)
             sys.exit(1)
         if args.ontop:
-            print(f"-x (--text) and -o (--ontop) are mutually exclusive")
+            print(f"[-] -x (--text) and -o (--ontop) are mutually exclusive", file=sys.stderr)
             sys.exit(1)
 
         args.headless = True
@@ -197,7 +176,7 @@ if __name__ == '__main__':
 
     s1, s2 = socket.socketpair()
 
-    run_qemu(s1, s2, getattr(args, 'app.elf'), args.library, args.seed, args.debug, args.trace, args.model, args.rampage, args.pagesize, args.sdk)
+    run_qemu(s1, s2, getattr(args, 'app.elf'), args.library, args.seed, args.debug, args.trace, args.model, args.rampage, args.sdk)
     s1.close()
 
     apdu = apdu_server.ApduServer(host="0.0.0.0", port=args.apdu_port)

src/launcher.c
@@ -617,12 +617,9 @@ static sdk_version_t str2sdkver(char *arg)
 
 static void usage(char *argv0)
 {
-  fprintf(stderr, "Usage: %s [-t] [-r <rampage_address> -s <rampage_size> -k <sdk_version>] <app.elf> [libname:lib.elf:0x1000:0x9fc0:0x20001800:0x1800 ...]\n", argv0);
+  fprintf(stderr, "Usage: %s [-t] [-r <rampage:ramsize> -k <sdk_version>] <app.elf> [libname:lib.elf:0x1000:0x9fc0:0x20001800:0x1800 ...]\n", argv0);
   fprintf(stderr, "\n\
-  -r <rampage_address>: Hex address (prefixed with '0x') of extra ram page to map\n\
-                        app.elf memory. Requires '-s' parameter.\n\
-  -s <rampage_size>:    Size in bytes of the extra RAM page provided by '-r'.\n\
-                        Value must be provided in hex, prefixed with '0x'.\n\
+  -r <rampage:ramsize>: Address and size of extra ram (both in hex) to map app.elf memory.\n\
   -m <model>:           Optional string representing the device model being emula-\n\
                         ted. Currently supports \"nanos\" and \"blue\".\n\
   -k <sdk_version>:     A string representing the SDK version to be used, like \"1.6\".\n");
@@ -649,10 +646,8 @@ int main(int argc, char *argv[])
       trace_syscalls = true;
       break;
     case 'r':
-      sscanf(optarg, "%p", &extra_rampage_addr);
-      break;
-    case 's':
-      sscanf(optarg, "%x", &extra_rampage_size);
+      if (sscanf(optarg, "%p:%x", &extra_rampage_addr, &extra_rampage_size) != 2)
+        errx(1, "invalid extram ram page/size\n");
       break;
     case 'm':
       if (!strcmp(optarg, "nanos")) {

commit 5a9e3fb65b9fd8bb2ea86530be5aa024803e4231

fix RAM page gathering from ELF binaries
Regression introduced by a6f7b65f32c0cd7a6fbe6f48c82d279b5ce8b36d.

speculos.py
@@ -82,6 +82,7 @@ def run_qemu(s1, s2, app_path, libraries=[], seed=DEFAULT_SEED, debug=False, tra
             if extra_ram and arg != extra_ram:
                 print("[-] Error: different extra RAM pages for main app and/or libraries!")
                 sys.exit(1)
+            extra_ram = arg
         args.append(f'{name}:{lib_path}:{hex(load_offset)}:{hex(load_size)}:{hex(stack)}:{hex(stack_size)}')
 
     if model == 'blue':