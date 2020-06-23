ed7be61c622a64a6bb125e580e8ae3de8ae540a5
Nicolas Iooss <nicolas.iooss@ledger.fr>
2020-06-23 14:57:48 +0200

    Test syscalls cx_hash_sha256 and cx_hash_sha512
    The syscalls `cx_hash_sha256` and `cx_hash_sha512` perform `hash_init`,
    `hash_update` and `hash_final` in a single syscall, and return the size
    of the digest. Test their implementations.

--- tests/syscalls/nist_cavp.h
+++ tests/syscalls/nist_cavp.h
@@ -10,3 +10,9 @@ void test_cavp_monte(cx_md_t md_type, uint8_t *initial_seed, const uint8_t *expe
 void test_cavp_short_msg_with_size(const char *filename, cx_md_t md_type, size_t digest_size);
 void test_cavp_long_msg_with_size(const char *filename, cx_md_t md_type, size_t digest_size);
 void test_cavp_monte_with_size(cx_md_t md_type, uint8_t *initial_seed, const uint8_t *expected_seed, size_t digest_size);
+
+/* Test with a single hash function, that combines init, update and final */
+typedef int (*single_hash_t)(const unsigned char *in, unsigned int len,
+                             unsigned char *out, unsigned int out_len);
+void test_cavp_short_msg_with_single(const char *filename, single_hash_t hash,
+                                     size_t digest_size);
