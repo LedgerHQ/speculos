e887b9a4fdc0ceb7eb01b35ccb43548f6bbe3fd2
greenknot <greenknot@users.noreply.github.com>
2021-07-09 15:10:37 +0200

    tests: add screenshots to the btc tests

--- tests/apps/test_btc.py
+++ tests/apps/test_btc.py
@@ -5,11 +5,14 @@ Tests to ensure that speculos launches correctly the BTC apps.
 '''
 
 import json
+import io
 import os
 import pkg_resources
 import pytest
 
 from enum import IntEnum
+
+import speculos.client
 
 CLA = 0xE0
 
@@ -38,15 +41,35 @@ def test_btc_get_version(client):
 
 
 def test_btc_get_public_key_with_user_approval(client, app):
-    if app.model != "blue":
-        pytest.skip(f"skipped on model {app.model}")
+    """Ask for the public key, compare screenshots and press reject."""
+
+    if app.model == "nanos" and app.hash == "00000000":
+        pytest.skip(f"skipped on model {app.model} because of animated text")
 
     bip32_path = bytes.fromhex("8000002C80000000800000000000000000000000")
     payload = bytes([len(bip32_path) // 4]) + bip32_path
 
     with client.apdu_exchange_async(CLA, Ins.GET_PUBLIC_KEY, payload, p1=0x01) as response:
-        client.finger_touch(220, 440)
-        data = response.receive()
+        if app.model == "blue":
+            client.wait_for_text_event("CONFIRM ACCOUNT")
+        else:
+            event = client.wait_for_text_event("Address")
+            while "Reject" not in event["text"]:
+                client.press_and_release("right")
+                event = client.get_next_event()
+
+        screenshot = client.get_screenshot()
+        path = pkg_resources.resource_filename(__name__, f"resources/btc_getpubkey_{app.model}.png")
+        assert speculos.client.screenshot_equal(path, io.BytesIO(screenshot))
+
+        if app.model == "blue":
+            client.finger_touch(110, 440)
+        else:
+            client.press_and_release("both")
+
+        with pytest.raises(speculos.client.ApduException) as e:
+            response.receive()
+        assert e.value.sw == 0x6985
 
 
 def test_btc_automation(client, app):
