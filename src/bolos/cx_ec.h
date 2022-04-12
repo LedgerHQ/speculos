3e75fca2f5f34e501b90814c9ef1140f6914db75
greenknot <greenknot@users.noreply.github.com>
2022-04-12 14:05:11 +0200

    merge cx_ec_sdk2.h into cx_ec.h
    Some functions and structures were defined in both header files, leading to
    errors when including cx_ec.h and cx_ec_sdk2.h.
    
    Having a single header file solves this issue.

--- src/bolos/cx_ec.h
+++ src/bolos/cx_ec.h
@@ -11,6 +11,7 @@
 #define CX_ECSCHNORR_LIBSECP     (6 << 12)
 #define CX_ECSCHNORR_Z           (7 << 12)
 
+#ifndef _SDK_2_0_
 /** List of supported elliptic curves */
 enum cx_curve_e {
   CX_CURVE_NONE,
@@ -81,12 +82,165 @@ enum cx_curve_e {
   CX_CURVE_MONTGOMERY_END
   /** High limit (not included) of Montgomery curve ID */
 };
+#else
+#define HAVE_SECP256K1_CURVE
+#define HAVE_SECP256R1_CURVE
+#define HAVE_SECP384R1_CURVE
+#define HAVE_SECP521R1_CURVE
+
+#define HAVE_BRAINPOOL_P256R1_CURVE
+#define HAVE_BRAINPOOL_P256T1_CURVE
+#define HAVE_BRAINPOOL_P320R1_CURVE
+#define HAVE_BRAINPOOL_P320T1_CURVE
+#define HAVE_BRAINPOOL_P384R1_CURVE
+#define HAVE_BRAINPOOL_P384T1_CURVE
+#define HAVE_BRAINPOOL_P512R1_CURVE
+#define HAVE_BRAINPOOL_P512T1_CURVE
+
+#define HAVE_ED25519_CURVE
+#define HAVE_ED448_CURVE
+
+#define HAVE_CV25519_CURVE
+#define HAVE_CV448_CURVE
+
+/** List of supported elliptic curves */
+enum cx_curve_e {
+  CX_CURVE_NONE,
+
+  /* ------------------------ */
+  /* --- Type Weierstrass --- */
+  /* ------------------------ */
+  /** Low limit (not included) of Weierstrass curve ID */
+  CX_CURVE_WEIERSTRASS_START = 0x20,
+
+/** Secp.org */
+#ifdef HAVE_SECP256K1_CURVE
+  CX_CURVE_SECP256K1 = 0x21,
+#define CX_CURVE_256K1 CX_CURVE_SECP256K1
+#endif
+
+#ifdef HAVE_SECP256R1_CURVE
+  CX_CURVE_SECP256R1 = 0x22,
+#define CX_CURVE_256R1    CX_CURVE_SECP256R1
+#define CX_CURVE_NISTP256 CX_CURVE_SECP256R1
+#endif
+
+#ifdef HAVE_SECP384R1_CURVE
+  CX_CURVE_SECP384R1 = 0x23,
+#define CX_CURVE_NISTP384 CX_CURVE_SECP384R1
+#endif
+
+#ifdef HAVE_SECP521R1_CURVE
+  CX_CURVE_SECP521R1 = 0x24,
+#define CX_CURVE_NISTP521 CX_CURVE_SECP521R1
+#endif
+
+#ifdef HAVE_BRAINPOOL_P256T1_CURVE
+  /** BrainpoolP256t1 */
+  CX_CURVE_BrainPoolP256T1 = 0x31,
+#endif
+
+#ifdef HAVE_BRAINPOOL_P256R1_CURVE
+  /** BrainpoolP256r1 */
+  CX_CURVE_BrainPoolP256R1 = 0x32,
+#endif
+
+#ifdef HAVE_BRAINPOOL_P320T1_CURVE
+  /** BrainpoolP320t1 */
+  CX_CURVE_BrainPoolP320T1 = 0x33,
+#endif
+
+#ifdef HAVE_BRAINPOOL_P320R1_CURVE
+  /** BrainpoolP320r1 */
+  CX_CURVE_BrainPoolP320R1 = 0x34,
+#endif
+
+#ifdef HAVE_BRAINPOOL_P384T1_CURVE
+  /** BrainpoolP384t1 */
+  CX_CURVE_BrainPoolP384T1 = 0x35,
+#endif
+
+#ifdef HAVE_BRAINPOOL_P384R1_CURVE
+  /** Brainpool384r1 */
+  CX_CURVE_BrainPoolP384R1 = 0x36,
+#endif
+
+#ifdef HAVE_BRAINPOOL_P512T1_CURVE
+  /** BrainpoolP512t1 */
+  CX_CURVE_BrainPoolP512T1 = 0x37,
+#endif
+
+#ifdef HAVE_BRAINPOOL_P512R1_CURVE
+  /** BrainpoolP512r1 */
+  CX_CURVE_BrainPoolP512R1 = 0x38,
+#endif
+
+  CX_CURVE_BLS12_381_G1 = 0x39,
+
+  /** High limit (not included) of Weierstrass curve ID */
+  CX_CURVE_WEIERSTRASS_END = 0x6F,
+
+  /* ---------------------------- */
+  /* --- Type Twister Edwards --- */
+  /* ---------------------------- */
+  /** Low limit (not included) of  Twister Edwards curve ID */
+  CX_CURVE_TWISTED_EDWARDS_START = 0x70,
+
+/** Ed25519 curve */
+#ifdef HAVE_ED25519_CURVE
+  CX_CURVE_Ed25519 = 0x71,
+#endif
+
+#ifdef HAVE_ED25519_CURVE
+  CX_CURVE_Ed448 = 0x72,
+#endif
+
+  CX_CURVE_TWISTED_EDWARDS_END = 0x7F,
+  /** High limit (not included) of Twister Edwards  curve ID */
+
+  /* ----------------------- */
+  /* --- Type Montgomery --- */
+  /* ----------------------- */
+  /** Low limit (not included) of Montgomery curve ID */
+  CX_CURVE_MONTGOMERY_START = 0x80,
+
+/** Curve25519 curve */
+#ifdef HAVE_CV25519_CURVE
+  CX_CURVE_Curve25519 = 0x81,
+#endif
+#ifdef HAVE_CV448_CURVE
+  CX_CURVE_Curve448 = 0x82,
+#endif
+
+  CX_CURVE_MONTGOMERY_END = 0x8F
+
+  /** High limit (not included) of Montgomery curve ID */
+};
+#endif
+
+#define CX_CURVE_RANGE(i, dom)                                                 \
+  (((i) > (CX_CURVE_##dom##_START)) && ((i) < (CX_CURVE_##dom##_END)))
+
+/** Return true if curve is a short Weierstrass curve */
+#define CX_CURVE_IS_WEIERSTRASS(c)                                             \
+  (((c) > CX_CURVE_WEIERSTRASS_START) && ((c) < CX_CURVE_WEIERSTRASS_END))
+
+/** Return true if curve is a twisted Edwards curve */
+#define CX_CURVE_IS_TWISTED_EDWARDS(c)                                         \
+  (((c) > CX_CURVE_TWISTED_EDWARDS_START) &&                                   \
+   ((c) < CX_CURVE_TWISTED_EDWARDS_END))
+
+/** Return true if curve is a Montgomery curve */
+#define CX_CURVE_IS_MONTGOMERY(c)                                              \
+  (((c) > CX_CURVE_MONTGOMERY_START) && ((c) < CX_CURVE_MONTGOMERY_END))
+
 /** Convenience type. See #cx_curve_e. */
 typedef enum cx_curve_e cx_curve_t;
 
 #define CX_ECCINFO_PARITY_ODD 1
 #define CX_ECCINFO_xGTn       2
 
+#ifndef _SDK_2_0_
 #define CX_CURVE_HEADER                                                        \
   /** Curve Identifier. See #cx_curve_e */                                     \
   cx_curve_t curve;                                                            \
@@ -107,12 +261,41 @@ typedef enum cx_curve_e cx_curve_t;
   /** @internal 2nd Mongtomery constant for Curve order*/                      \
   const unsigned char *Hn;                                                     \
   /**  cofactor */                                                             \
-  int h
+  int h;
+#else
+
+#define CX_CURVE_HEADER                                                        \
+  /** Curve Identifier. See #cx_curve_e */                                     \
+  cx_curve_t curve;                                                            \
+  /** Curve size in bits */                                                    \
+  unsigned int bit_size;                                                       \
+  /** component length in bytes */                                             \
+  unsigned int length;                                                         \
+  /**  a coef */                                                               \
+  const uint8_t *a;                                                            \
+  /**  b/d coef */                                                             \
+  const uint8_t *b;                                                            \
+  /** Curve field */                                                           \
+  const uint8_t *p;                                                            \
+  /** Point Generator x coordinate */                                          \
+  const uint8_t *Gx;                                                           \
+  /** Point Generator y coordinate */                                          \
+  const uint8_t *Gy;                                                           \
+  /** Curve order */                                                           \
+  const uint8_t *n;                                                            \
+  /**  cofactor */                                                             \
+  const uint8_t *h;                                                            \
+  /** @internal 2nd Montgomery constant for Curve order */                     \
+  const uint8_t *Hn;                                                           \
+  /** @internal 2nd Montgomery constant for Field */                           \
+  const uint8_t *Hp;
+#endif
 
 /**
  * Weirstrass curve :     y^3=x^2+a*x+b        over F(p)
  *
  */
+#ifndef _SDK_2_0_
 struct cx_curve_weierstrass_s {
   CX_CURVE_HEADER;
   /**  a coef */
@@ -120,9 +303,6 @@ struct cx_curve_weierstrass_s {
   /**  b coef */
   const unsigned char *b;
 };
-
-/** Convenience type. See #cx_curve_weierstrass_s. */
-typedef struct cx_curve_weierstrass_s cx_curve_weierstrass_t;
 
 /*
  * Twisted Edward curve : a*x^2+y^2=1+d*x2*y2  over F(q)
@@ -138,8 +318,23 @@ struct cx_curve_twisted_edward_s {
   /** @internal  (q+3)/8 or (q+1)/4*/
   const unsigned char *Qq;
 };
+
 /** Convenience type. See #cx_curve_twisted_edward_s. */
 typedef struct cx_curve_twisted_edward_s cx_curve_twisted_edward_t;
+#else
+struct cx_curve_weierstrass_s {
+  CX_CURVE_HEADER
+};
+
+struct cx_curve_twisted_edwards_s {
+  CX_CURVE_HEADER
+};
+
+typedef struct cx_curve_twisted_edwards_s cx_curve_twisted_edwards_t;
+#endif
+
+/** Convenience type. See #cx_curve_weierstrass_s. */
+typedef struct cx_curve_weierstrass_s cx_curve_weierstrass_t;
 
 struct cx_curve_domain_s {
   CX_CURVE_HEADER;
@@ -266,6 +461,43 @@ struct cx_ecfp_640_private_key_s {
 typedef struct cx_ecfp_640_public_key_s cx_ecfp_640_public_key_t;
 /** Convenience type. See #cx_ecfp_640_private_key_s. */
 typedef struct cx_ecfp_640_private_key_s cx_ecfp_640_private_key_t;
+
+/*
+ * Montgomery curve: B*y^2= x^3 + A*x^2 + x over F(q)
+ */
+struct cx_curve_montgomery_s {
+  CX_CURVE_HEADER
+};
+/** Convenience type. See #cx_curve_montgomery_s. */
+typedef struct cx_curve_montgomery_s cx_curve_montgomery_t;
+
+// cx_bn_t is an index on the corresponding cx_mpi_array;
+typedef uint32_t cx_bn_t;
+
+/**
+ *
+ */
+struct cx_ec_point_s {
+  cx_curve_t curve;
+  cx_bn_t x;
+  cx_bn_t y;
+  cx_bn_t z;
+};
+
+/** Convenience type. See #cx_ec_point_s. */
+typedef struct cx_ec_point_s cx_ecpoint_t;
+
+enum cx_curve_dom_param_s {
+  CX_CURVE_PARAM_NONE = 0,
+  CX_CURVE_PARAM_A = 1,
+  CX_CURVE_PARAM_B = 2,
+  CX_CURVE_PARAM_Field = 3,
+  CX_CURVE_PARAM_Gx = 4,
+  CX_CURVE_PARAM_Gy = 5,
+  CX_CURVE_PARAM_Order = 6,
+  CX_CURVE_PARAM_Cofactor = 7,
+};
+typedef enum cx_curve_dom_param_s cx_curve_dom_param_t;
 
 const cx_curve_domain_t *cx_ecfp_get_domain(cx_curve_t curve);
 
