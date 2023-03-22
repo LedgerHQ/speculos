053450b5479dcb27cb193afc267eee5e9640c031
Xavier Chapron <xavier.chapron@ledger.fr>
2023-03-22 17:00:43 +0100

    src: bolos: os.c: Reverse sys_os_setting_get() check to avoid having warning on new sdk

--- src/bolos/os.c
+++ src/bolos/os.c
@@ -43,14 +43,17 @@ unsigned long sys_os_setting_get(unsigned int setting_id,
                                  uint8_t *UNUSED(value), size_t UNUSED(maxlen))
 {
   // Since Nano X SDK 2.0 & Nano S SDK 2.1, OS_SETTING_PLANEMODE is 6!
-  if (sdk_version == SDK_NANO_X_2_0 || sdk_version == SDK_NANO_X_2_0_2 ||
-      sdk_version == SDK_NANO_S_2_1) {
-    if (setting_id == OS_SETTING_PLANEMODE_NEW) {
+  if (sdk_version == SDK_NANO_X_1_2 || sdk_version == SDK_NANO_S_1_5 ||
+      sdk_version == SDK_NANO_S_1_6 || sdk_version == SDK_NANO_S_2_0 ||
+      sdk_version == SDK_BLUE_1_5 || sdk_version == SDK_BLUE_2_2_5 ||
+      sdk_version == SDK_NANO_SP_1_0 || sdk_version == SDK_NANO_SP_1_0_3) {
+    if (setting_id == OS_SETTING_PLANEMODE_OLD) {
       return 1;
     }
-  } else if (setting_id == OS_SETTING_PLANEMODE_OLD) {
+  } else if (setting_id == OS_SETTING_PLANEMODE_NEW) {
     return 1;
   }
+
   fprintf(stderr, "os_setting_get not implemented for 0x%x\n", setting_id);
 
   return 0;
