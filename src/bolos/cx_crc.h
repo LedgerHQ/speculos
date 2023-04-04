19754ba54e574a9e4aa3cb9e2274dff48a5e8361
Aido <aido@users.noreply.github.com>
2023-04-05 00:37:42 +0100

    Correct the implementation of CRC32 fixes #363

--- src/bolos/cx_crc.h
+++ src/bolos/cx_crc.h
@@ -4,3 +4,5 @@
 
 unsigned short sys_cx_crc16_update(unsigned short crc, const void *buf,
                                    size_t len);
+
+unsigned int sys_cx_crc32_update(unsigned int crc, const void *buf, size_t len);
