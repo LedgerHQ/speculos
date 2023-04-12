bcd4536ae9a10690189704ad11be7ac937abc0c1
abonnaudet-ledger <arthur.bonnaudet@ledger.fr>
2023-04-12 15:39:24 +0200

    Fat: 4BPP color maps are managed only on nbgl.py

--- src/bolos/nbgl_rle.c
+++ src/bolos/nbgl_rle.c
@@ -1,48 +1,6 @@
 #include "nbgl_rle.h"
 #include <string.h>
 
-// 4BPP Color maps
-
-// Extract a color from color_map, if color_map is not NULL
-#define GET_MAPPED_COLOR(color_map, color_index)                               \
-  ((color_map == NULL) ? (color_index) : color_map[(color_index)])
-
-// clang-format off
-static const uint8_t WHITE_ON_BLACK[] = {
-    WHITE_4BPP, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, BLACK_4BPP
-};
-
-static const uint8_t DARK_GRAY_ON_WHITE[] = {
-    DARK_GRAY_4BPP, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 11, 12, 13, 14, WHITE_4BPP
-};
-
-static const uint8_t LIGHT_GRAY_ON_WHITE[] = {
-    LIGHT_GRAY_4BPP, 10, 11, 11, 12, 12, 13, 13, 13, 14, 14, 14, 15, 15, 15, WHITE_4BPP
-};
-
-static const uint8_t LIGHT_GRAY_ON_BLACK[] = {
-    LIGHT_GRAY_4BPP, 9, 8, 7, 6, 5, 4, 4, 3, 3, 2, 2, 1, 1, 0, BLACK_4BPP
-};
-// clang-format on
-
-static const uint8_t *get_color_map_array(nbgl_area_t *area, color_t color)
-{
-  if ((color == BLACK) && (area->backgroundColor == WHITE)) {
-    return NULL;
-  } else if ((color == WHITE) && (area->backgroundColor == BLACK)) {
-    return WHITE_ON_BLACK;
-  } else if ((color == DARK_GRAY) && (area->backgroundColor == WHITE)) {
-    return DARK_GRAY_ON_WHITE;
-  } else if ((color == LIGHT_GRAY) && (area->backgroundColor == WHITE)) {
-    return LIGHT_GRAY_ON_WHITE;
-  } else if ((color == LIGHT_GRAY) && (area->backgroundColor == BLACK)) {
-    return LIGHT_GRAY_ON_BLACK;
-  } else if ((color == DARK_GRAY) && (area->backgroundColor == BLACK)) {
-    return LIGHT_GRAY_ON_BLACK;
-  }
-  return NULL;
-}
-
 // Write nb_pix 4BPP pixels of the same color to the display
 static inline void fill_4bpp_pixels_color(uint8_t color, uint8_t nb_pix,
                                           uint32_t *pix_cnt, uint8_t *remaining,
@@ -85,13 +43,12 @@ static inline void fill_4bpp_pixels_color(uint8_t color, uint8_t nb_pix,
 
 // Handle 'Copy white' RLE instruction
 static uint32_t handle_4bpp_repeat_white(uint8_t byte_in, uint32_t *pix_cnt,
-                                         const uint8_t *color_map,
                                          uint8_t *remaining_byte,
                                          uint32_t max_pix_cnt,
                                          uint8_t *out_buffer)
 {
   uint8_t nb_pix = (byte_in & 0x3F) + 1;
-  uint8_t color = GET_MAPPED_COLOR(color_map, 0x0F);
+  uint8_t color = WHITE_4BPP;
   fill_4bpp_pixels_color(color, nb_pix, pix_cnt, remaining_byte, max_pix_cnt,
                          out_buffer);
   return 1;
@@ -99,13 +56,12 @@ static uint32_t handle_4bpp_repeat_white(uint8_t byte_in, uint32_t *pix_cnt,
 
 // Handle 'Repeat color' RLE instruction
 static uint32_t handle_4bpp_repeat_color(uint8_t byte_in, uint32_t *pix_cnt,
-                                         const uint8_t *color_map,
                                          uint8_t *remaining_byte,
                                          uint32_t max_pix_cnt,
                                          uint8_t *out_buffer)
 {
   uint8_t nb_pix = ((byte_in & 0x70) >> 4) + 1;
-  uint8_t color = GET_MAPPED_COLOR(color_map, byte_in & 0x0F);
+  uint8_t color = byte_in & 0x0F;
   fill_4bpp_pixels_color(color, nb_pix, pix_cnt, remaining_byte, max_pix_cnt,
                          out_buffer);
   return 1;
@@ -113,9 +69,8 @@ static uint32_t handle_4bpp_repeat_color(uint8_t byte_in, uint32_t *pix_cnt,
 
 // Handle 'Copy' RLE instruction
 static uint32_t handle_4bpp_copy(uint8_t *bytes_in, uint32_t bytes_in_len,
-                                 uint32_t *pix_cnt, const uint8_t *color_map,
-                                 uint8_t *remaining_byte, uint32_t max_pix_cnt,
-                                 uint8_t *out_buffer)
+                                 uint32_t *pix_cnt, uint8_t *remaining_byte,
+                                 uint32_t max_pix_cnt, uint8_t *out_buffer)
 {
   uint8_t nb_pix = ((bytes_in[0] & 0x30) >> 4) + 3;
   uint8_t nb_bytes_read = (nb_pix / 2) + 1;
@@ -134,8 +89,8 @@ static uint32_t handle_4bpp_copy(uint8_t *bytes_in, uint32_t bytes_in_len,
     } else {
       color_index = bytes_in[index] >> 4;
     }
-    uint8_t color = GET_MAPPED_COLOR(color_map, color_index);
-    fill_4bpp_pixels_color(color, 1, pix_cnt, remaining_byte, max_pix_cnt,
+
+    fill_4bpp_pixels_color(color_index, 1, pix_cnt, remaining_byte, max_pix_cnt,
                            out_buffer);
   }
 
@@ -159,15 +114,14 @@ static uint32_t handle_4bpp_copy(uint8_t *bytes_in, uint32_t bytes_in_len,
  * @param area
  * @param buffer buffer of RLE-encoded data
  * @param buffer_len length of buffer
- * @param fore_color
  * @param out_buffer output buffer
  * @param out_buffer_len length of output buffer
  * @return
  */
 
 void nbgl_uncompress_rle_4bpp(nbgl_area_t *area, uint8_t *buffer,
-                              uint32_t buffer_len, color_t fore_color,
-                              uint8_t *out_buffer, uint32_t out_buffer_len)
+                              uint32_t buffer_len, uint8_t *out_buffer,
+                              uint32_t out_buffer_len)
 {
   if (area->bpp != NBGL_BPP_4) {
     return;
@@ -181,7 +135,6 @@ void nbgl_uncompress_rle_4bpp(nbgl_area_t *area, uint8_t *buffer,
 
   memset(out_buffer, 0, out_buffer_len);
 
-  const uint8_t *color_map = get_color_map_array(area, fore_color);
   uint32_t pix_cnt = 0;
   uint32_t read_cnt = 0;
   uint8_t remaining = 0;
@@ -190,17 +143,17 @@ void nbgl_uncompress_rle_4bpp(nbgl_area_t *area, uint8_t *buffer,
     uint8_t byte = buffer[read_cnt];
     if (byte & 0x80) {
       if (byte & 0x40) {
-        read_cnt += handle_4bpp_repeat_white(
-            byte, &pix_cnt, color_map, &remaining, max_pix_cnt, out_buffer);
+        read_cnt += handle_4bpp_repeat_white(byte, &pix_cnt, &remaining,
+                                             max_pix_cnt, out_buffer);
       } else {
         uint8_t *bytes_in = buffer + read_cnt;
         uint32_t max_len = buffer_len - read_cnt;
-        read_cnt += handle_4bpp_copy(bytes_in, max_len, &pix_cnt, color_map,
-                                     &remaining, max_pix_cnt, out_buffer);
+        read_cnt += handle_4bpp_copy(bytes_in, max_len, &pix_cnt, &remaining,
+                                     max_pix_cnt, out_buffer);
       }
     } else {
-      read_cnt += handle_4bpp_repeat_color(byte, &pix_cnt, color_map,
-                                           &remaining, max_pix_cnt, out_buffer);
+      read_cnt += handle_4bpp_repeat_color(byte, &pix_cnt, &remaining,
+                                           max_pix_cnt, out_buffer);
     }
   }
 
