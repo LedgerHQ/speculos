8b4b74886c53b418f6145f22c456bada5f154bf4
sra <sandra.rasoamiaramanana@ledger.fr>
2023-04-26 12:00:27 +0200

    Add BLS syscalls and unitary tests

---
+++ src/bolos/cx_bls.h
@@ -0,0 +1,37 @@
+#ifndef CX_BLS_H
+#define CX_BLS_H
+
+#include <blst.h>
+#include <stdint.h>
+
+#define CX_BLS_BLS12381_PARAM_LEN (48)
+#define CX_BLS_BLS12381_KEY_LEN   (32)
+#define BLS_FIELD_EXT_LEN         (CX_BLS_BLS12381_PARAM_LEN * 2)
+#define BLS_COMPRESSED_SIG_LEN    (BLS_FIELD_EXT_LEN)
+#define CX_BLS_PROVE              (0x9)
+#define CX_BLS_BASE               (0x0)
+#define CX_BLS_SIGN_AUG           (0xA)
+#define CX_BLS_SIGN_POP           (0xF)
+
+void cx_bls_fp2_from_bendian(blst_fp2 *ret, uint8_t c0[48], uint8_t c1[48]);
+void cx_bls_bendian_from_fp2(uint8_t c0[48], uint8_t c1[48], blst_fp2 *a);
+
+void cx_bls_fp2_conditionnal_move(blst_fp2 *ret, blst_fp2 *a, blst_fp2 *b,
+                                  bool choice);
+void cx_bls_fp2_copy(blst_fp2 *ret, blst_fp2 *a);
+void cx_bls_fp2_set_one(blst_fp2 *a);
+bool cx_bls_fp2_is_zero(blst_fp2 *a);
+uint8_t cx_bls_fp2_sgn0(blst_fp2 *a);
+void cx_bls_fp2_frobenius(blst_fp2 *ret, blst_fp2 *a);
+void cx_bls_fp2_pow(blst_fp2 *ret, blst_fp2 *a, uint8_t *exp, size_t exp_len);
+bool cx_bls_fp2_sqrt_ratio(blst_fp2 *ret, blst_fp2 *u, blst_fp2 *v);
+
+void cx_bls_g2_psi(blst_p2_affine *ret_point, blst_p2_affine *point);
+void cx_bls_g2_psi2(blst_p2_affine *ret_point, blst_p2_affine *point);
+void cx_bls_g2_map_to_curve_simple_swu(blst_p2_affine *point, blst_fp2 *u);
+void cx_bls_g2_iso_map_3(blst_p2_affine *ret_point, blst_p2_affine *point);
+void cx_bls_g2_clear_cofactor(blst_p2_affine *ret_point, blst_p2_affine *point);
+void cx_bls_g2_hash_field_to_curve(blst_p2_affine *point, uint8_t *hash,
+                                   size_t hash_len);
+
+#endif /* CX_BLS_H */
--- src/bolos/cx_hkdf.c
+++ src/bolos/cx_hkdf.c
@@ -1,6 +1,6 @@
+#define _SDK_2_0_
+#include "cxlib.h"
 #include <string.h>
-
-#include "cx.h"
 
 // HMAC-Based Key Kerivation Function defined at
 // https://tools.ietf.org/html/rfc5869 Used to implement the EIP-2333 for
@@ -53,5 +53,61 @@ void spec_cx_hkdf_expand(const cx_md_t hash_id, const unsigned char *prk,
     offset = (okm_len < md_len) ? okm_len : md_len;
     memcpy(okm + (i - 1) * md_len, T, offset);
     okm_len -= offset;
+  }
+}
+
+// Hash 32 random bytes into the subgroup of the BLS12-381 privates keys
+// Input: ikm = a secret octet string >= 256 bits: ikm = ikm || I2OSP(0, 1)
+//        ikm_len = ikm length
+//        key_info = an optional string: key_info = key_info || I2OSP(L, 2)
+//        key_info_len = key_info length
+// Output: sk = the secret key, an integer s.t 0 <= sk < r, r is the subgroup
+// order
+// 1. salt = "BLS-SIG-KEYGEN-SALT-"
+// 2. SK = 0
+// 3. while SK == 0:
+// 4.     salt = SHA256(salt)
+// 5.     PRK = HKDF-Extract(salt, IKM || I2OSP(0, 1))
+// 6.     OKM = HKDF-Expand(PRK, key_info || I2OSP(L, 2), L)
+// 7.     SK = OS2IP(OKM) mod r
+// 8. return SK
+
+void cx_hkdf_mod_r(const unsigned char *ikm, unsigned int ikm_len,
+                   const uint8_t *salt, unsigned int salt_len,
+                   unsigned char *key_info, unsigned int key_info_len,
+                   unsigned char *sk)
+{
+  unsigned char prk[CX_SHA256_SIZE];
+  unsigned char okm[CX_HKDF_MOD_R_CEIL];
+  unsigned char HKDF_MOD_R_SALT[] = { 'B', 'L', 'S', '-', 'S', 'I', 'G',
+                                      '-', 'K', 'E', 'Y', 'G', 'E', 'N',
+                                      '-', 'S', 'A', 'L', 'T', '-' };
+  const cx_curve_domain_t *domain;
+  unsigned char key_salt[CX_SHA256_SIZE] = { 0 };
+  unsigned char used_salt[CX_SHA256_SIZE] = { 0 };
+  unsigned int used_salt_len;
+
+  domain = cx_ecdomain(CX_CURVE_BLS12_381_G1);
+
+  if (NULL == salt) {
+    sys_cx_hash_sha256(HKDF_MOD_R_SALT, sizeof(HKDF_MOD_R_SALT), key_salt,
+                       CX_SHA256_SIZE);
+    used_salt_len = CX_SHA256_SIZE;
+  } else {
+    used_salt_len = (salt_len <= CX_SHA256_SIZE) ? salt_len : CX_SHA256_SIZE;
+    memcpy(key_salt, salt, used_salt_len);
+  }
+
+  memset(sk, 0, KEY_LENGTH);
+
+  while (cx_math_is_zero(sk, KEY_LENGTH)) {
+    cx_hkdf_extract(CX_SHA256, ikm, ikm_len, key_salt, used_salt_len, prk);
+    spec_cx_hkdf_expand(CX_SHA256, prk, CX_SHA256_SIZE, key_info, key_info_len,
+                        okm, CX_HKDF_MOD_R_CEIL);
+    cx_math_mod(okm, CX_HKDF_MOD_R_CEIL, domain->n, CX_HKDF_MOD_R_CEIL);
+    memcpy(sk, okm + (CX_HKDF_MOD_R_CEIL - KEY_LENGTH), KEY_LENGTH);
+    memcpy(used_salt, key_salt, used_salt_len);
+    sys_cx_hash_sha256(used_salt, used_salt_len, key_salt, CX_SHA256_SIZE);
+    used_salt_len = CX_SHA256_SIZE;
   }
 }
