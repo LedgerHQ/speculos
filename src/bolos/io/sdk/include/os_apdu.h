287b43dd82e9ac74bf4bbb6ea8fb2491bf290365
Stephane Portron <stephane.portron@legder.fr>
2025-05-16 13:44:36 +0200

    IO revamp high level implementation

---
+++ src/bolos/io/sdk/include/os_apdu.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/* ----------------------------------------------------------------------- */
+/* -                       APDU-RELATED DEFINES                          - */
+/* ----------------------------------------------------------------------- */
+
+// Offsets within the APDU buffer.
+#define APDU_OFF_CLA  0
+#define APDU_OFF_INS  1
+#define APDU_OFF_P1   2
+#define APDU_OFF_P2   3
+#define APDU_OFF_LC   4
+#define APDU_OFF_DATA 5
