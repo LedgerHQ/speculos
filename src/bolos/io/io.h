983b999055e707e813d90c638adc9407353dd858
Stephane Portron <stephane.portron@legder.fr>
2025-05-16 13:44:36 +0200

    IO revamp high level implementation

--- src/bolos/io/io.h
+++ src/bolos/io/io.h
@@ -1,11 +1,8 @@
-#ifndef OS_IO_H_
-#define OS_IO_H_
+#pragma once
 
 #include <stdbool.h>
 
-typedef struct {
-  int init;
-} os_io_init_t;
+#include "os_io.h"
 
 int sys_os_io_init(os_io_init_t *init);
 
@@ -22,9 +19,6 @@ int sys_os_io_rx_evt(unsigned char *buffer, unsigned short buffer_max_length,
 int sys_os_io_seph_tx(const unsigned char *buffer, unsigned short length,
                       unsigned int *timeout_ms);
 
-int sys_os_io_seph_se_rx_event(unsigned char *buffer,
-                               unsigned short max_length,
-                               unsigned int  *timeout_ms,
-                               bool           check_se_event,
-                               unsigned int   flags);
-#endif
+int sys_os_io_seph_se_rx_event(unsigned char *buffer, unsigned short max_length,
+                               unsigned int *timeout_ms, bool check_se_event,
+                               unsigned int flags);
---
+++ src/bolos/io/mock/include/cx_rng_internal.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "os_utils.h"
+#define cx_rng_internal sys_cx_rng
