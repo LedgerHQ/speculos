4fd6c010cda16314987b7f9facbcfb11549bc645
Arthur Bonnaudet <arthur.bonnaudet@ledger.fr>
2026-04-20 13:20:31 +0200

    OCR: Introduce NBGL serialized events
    Starting with API_LEVEL_26, `NBGL serialized events` are now sent by the NBGL object layer though the
    shared libs.
    
    Those events are parsed and the contained texts are extracted 
    
    The OCR based on the fonts is not used for NBGL apps, starting with API_LEVEL_26.

--- speculos/mcu/ocr.py
+++ speculos/mcu/ocr.py
@@ -211,6 +211,12 @@ class OCR:
         # create a new TextEvent if there are no events yet or if there is a new line
         self.events.append(TextEvent(char, x, y, w, h, False))
 
+    def add_text(self, text: str) -> None:
+        """
+        Add a line of text as a TextEvent, to be sent to the client
+        """
+        self.events.append(TextEvent(text, 0, 0, 0, 0, False))
+
     def analyze_bitmap(self, data: bytes, use_bagl: bool) -> None:
         """
         data contain information about the latest displayed bitmap.
